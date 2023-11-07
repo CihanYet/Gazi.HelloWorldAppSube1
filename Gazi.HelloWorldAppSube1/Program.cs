5b25a270e9b5bba638a042dab1b0876c396a55d0
Gazi <Gazi@DESKTOP-224SE1B>
2023-11-07 15:16:24 +0300

    Add project files.

---
+++ Gazi.HelloWorldAppSube1/Program.cs
@@ -0,0 +1,160 @@
+﻿namespace Gazi.HelloWorldAppSube1
+{
+    internal class Program
+    {
+        static int sayi = 50;
+        static void Main(string[] args)
+        {
+            //byte a;
+            //a = 30;
+
+            //byte b= 30;
+            //Console.WriteLine(a);
+
+            //const int number = 100;
+            //Console.WriteLine(number);  
+            //number = 50;
+            //Console.WriteLine(number);  
+
+            //byte yas;
+            //Console.WriteLine(yas);
+
+            //byte s1 = 50;
+
+            //short s2 = 50;
+            //int s3 = 50;
+            //long s4 = 50;
+
+            //Int16 s5 = 50;  
+            //Int32 s6 = 50;
+            //Int64 s7 = 50;
+
+            //var isim = "Ahmet";
+            //byte sayi = 100;
+
+            //var harf = 'A';
+
+            //string isim = "Ahmet";
+            //string soyad = "Mehmet";
+
+            //Console.WriteLine("Merhaba " + isim + " " + soyad);
+            //Console.WriteLine("Merhaba {0} {1}", isim, soyad);
+            //Console.WriteLine($"Merhaba {isim} {soyad}");
+
+            //Console.WriteLine("İsminizi Giriniz:");
+            //string isim = Console.ReadLine();
+            //Console.WriteLine("Soyadınızı Giriniz:");
+            //string soyad = Console.ReadLine();
+            //Console.WriteLine("Yaşınızı giriniz:");
+            //byte yas = byte.Parse(Console.ReadLine());
+            //Console.WriteLine($"Hoşgeldin {isim} {soyad}");
+
+            //C# Tip Güvenliği (Type Safety) sağlayan bir programlama dilidir. Bu özellik, veri tipine uygun olmayan tipte değer atanmasının önüne geçer.Eğer bu değeri uygun hale getirerek atamak istersek, TÜR DÖNÜŞÜMÜ(Type Casting) yapmamız gerekir.
+
+            //double maas = 5000;
+            //maas += 1000;//maas=maas+1000
+            //Console.WriteLine(maas);
+
+            //int sayi1 = 10;
+            //int sayi2 = 20;
+
+            //string sonuc = sayi2 > sayi1 ? "Sayı 2 büyüktür." : "Sayı 1 büyüktür yada sayılar eşittir";
+            //Console.WriteLine(sonuc);
+
+
+            //TÜR DÖNÜŞÜMLERİ(Type Casting)
+
+            //byte sayi = 20;
+            //int number = sayi;//Implicit Type Casting
+
+
+            //checked
+            //{
+            //    int sayi = 255;
+            //    byte number = (byte)sayi;//Explicit Type Casting
+            //    Console.WriteLine(number); 
+            //}
+            //Büyük tür->Küçük tür RİSK: Değer kaybı
+
+            //string->değer türlerine 
+
+            //string sayi = "25";
+            ////int number = Convert.ToInt32(sayi);
+            //byte number = byte.Parse(sayi);
+
+
+
+            //Console.WriteLine("Bir sayı giriniz");
+            //int sayi1 = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Bir sayı daha giriniz");
+            //int sayi2 = int.Parse(Console.ReadLine());
+            //Console.WriteLine($"Girilen sayıların toplamı:{sayi1 + sayi2}");
+
+
+
+
+        }
+    }
+}
+
+//Bit: 0-1
+//8 Bit = 1 Byte
+//1024Byte = 1 Kilobyte  (KB)
+//1024KB = 1 Megabyte (MB)
+//1024MB = 1 Gigabyte(GB)
+//1024GB = 1 Terabyte(TB)
+
+
+//Binary: 2'li sayı sistemi
+//Decimal: 10 tabanlı
+
+/*
+-Namespace: İçinde classları bulunduran yapılardır.
+-{ }: Blok yapıları(Scope)
+-Class: İçinde metodları bulunduran yapılardır.
+-Metod: İş yapan kod bloklarıdır.
+-Main Metodu: Console uygulamalarının giriş noktasıdır.                      Bulunması zorunludur.
+-Syntax: Kod yazım kuralları
+-Build->Derleme(Compile): Programlama Dilinin makina diline çevrilmesi işlemi
+-Solution: İçinde birden fazla proje bulundurabilen yapılara solution denir.
+
+MS-IL: Microsoft Intermediate Language
+
+C#->Build->MS-IL(exe)->Runtime->JIT Compiler->Binary(0-1)
+Runtime: Çalışma zamanı 
+.Net Core->Core Common Language Runtime
+JIT: Just In Time
+Compiler: Derleyici
+
+.NET: Microsoft tarafından yazılım geliştirmek için tasarlanan bir framework. İçinde programlama dilleri,editörler,derleyiciler, kütüphaneler bulunur.
+
+- Programlama Dilleri- C#-Visual Basic-F#
+- Editör: Visual Studio-Visual Studio Code
+- Kütüphaneler(Library): İçinde classlar bulunur.
+- Derleyiciler: Kodları makina diline çevirmek için gerekli yazılımlar.
+
+- Desktop(Masaüstü) 
+- Web Based Software(Web tabanlı yazılım)
+- Mobile Application (Android-iOS) Xamarin ile
+- Game Software (Unity ile)
+- iOT: Internet Of Things (Akıllı Cihazlar için yazılımlar)
+- AI (Yapay Zeka) Uygulamaları yapılabilir.
+
+Cross Platform: Windows, MacOs, Linux işletim sistemlerinde çalışabilen uygulamalar. 
+
+.NET Framework: Windows
+.NET Core: Cross Platform
+.NET
+
+
+0 && 0 = 0
+0 && 1 = 0
+1 && 0 = 0
+1 && 1 = 1
+
+0 || 0 = 0
+0 || 1 = 1
+1 || 0 = 1
+1 || 1 = 1
+
+*/
---
+++ Gazi.KararYapilariApp/Program.cs
@@ -0,0 +1,97 @@
+﻿namespace Gazi.KararYapilariApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int s1 = 10;
+            //int s2 = 20;
+
+            //if (s1 > s2)
+            //{
+            //    Console.WriteLine("Sayı 1 büyüktür");
+            //}
+            //else if (s2 > s1)
+            //{
+            //    Console.WriteLine("Sayı 2 büyük");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Sayılar eşittir");
+            //}
+
+            //string msg1, msg2, msg3, msg4,msg5;
+
+            //Console.WriteLine("Dil Seçiniz/Select Language(TR/EN):");
+            //string cevap=Console.ReadLine();
+            //if (cevap.ToUpper()=="TR")
+            //{
+            //    msg1 = "Bir sayı giriniz:";
+            //    msg2 = "Bir sayı daha giriniz:";
+            //    msg3 = "İşlem seçiniz:";
+            //    msg4 = "İşlemin sonucu:";
+            //    msg5 = "Hatalı işlem seçtiniz.";
+            //}
+            //else
+            //{
+            //    msg1 = "Enter first number:";
+            //    msg2 = "Enter second number:";
+            //    msg3 = "Select operation:";
+            //    msg4 = "Result:";
+            //    msg5 = "Incorrect operation selected";
+            //}
+
+            //Console.WriteLine(msg1);
+            //double s1=double.Parse(Console.ReadLine());
+            //Console.WriteLine(msg2);
+            //double s2=double.Parse(Console.ReadLine());
+            //Console.WriteLine(msg3);
+            //char islem=char.Parse(Console.ReadLine());            
+            //if (islem=='+')
+            //{
+            //    Console.WriteLine($"{msg4}{s1+s2}");
+            //}
+            //else if(islem=='-')
+            //{
+            //    Console.WriteLine($"{msg4}{s1 - s2}");
+            //}
+            //else if(islem=='*')
+            //{
+            //    Console.WriteLine($"{msg4}{s1 * s2}");
+            //}
+            //else if (islem=='/')
+            //{
+            //    Console.WriteLine($"{msg4}{s1 / s2}");
+            //}
+            //else
+            //{
+            //    Console.WriteLine(msg5);
+            //}    
+
+            Console.WriteLine("Hava nasıl?");
+            string durum = Console.ReadLine().ToLower();
+
+            if (durum == "güzel" || durum=="iyi")
+            {
+                Console.WriteLine("Dışarı çık");
+            }
+            else if (durum == "kötü")
+            {
+                Console.WriteLine("Evde otur");
+            }
+            else
+            {
+                Console.WriteLine("Hatalı giriş");
+            }
+        }
+    }
+}
+
+//Dil seçiniz/Select Language:EN
+//Enter first number:10
+//Enter second number:20
+//Select Operation: +
+//Result: 30
+
+//Dil seçiniz/Select Language:TR
+
